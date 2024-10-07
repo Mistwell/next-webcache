1c7fe3f1472a3bb8b804cf08042b34bdeceffbb8
Илья Терёшин <terkin1805@gmail.com>
2024-10-07 13:40:19 +0400

    yandex

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ const roboto = Roboto({
 export const metadata: Metadata = {
 	title: "Nextjs Webcache",
 	description: "Cache test",
+	verification: {
+		yandex: "32aa2d44c3e7b0d3",
+	},
 };
 
 export default function RootLayout({
